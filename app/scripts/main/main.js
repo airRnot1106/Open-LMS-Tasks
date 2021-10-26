e2e39d220383da45513bbf1bef16a5c4b48fc83d
airRnot1106 <yngw.rintaro@gmail.com>
2021-10-26 18:04:11 +0900

    リファクタリング

--- app/scripts/main/main.js
+++ app/scripts/main/main.js
@@ -27,58 +27,81 @@ class LMSManager {
     async execute() {
         console.log('start');
         const url = location.href;
-        if (!this.isAllowUrl(url))
-            return;
         const id = this.extractId(url);
-        const cachesData = await LocalStorage.get();
-        const [, , , , type] = url.split('/');
-        let taskObj;
-        if (cachesData.hasOwnProperty(id)) {
-            taskObj = JSON.parse(cachesData[id]);
+        console.log('id:', id);
+        const type = url.split('/')[4];
+        let task;
+        switch (type) {
+            case 'assign':
+                task = new Assign(id);
+                break;
+            case 'quiz':
+                task = new Quiz(id);
         }
-        else {
-            switch (type) {
-                case 'assign':
-                    taskObj = this.fetchAssignData();
-                    break;
-                case 'quiz':
-                    taskObj = this.fetchQuizData();
-                    break;
-            }
-        }
-        if (!taskObj) {
-            return;
-        }
-        this.checkStatUpdate(taskObj);
-        await LocalStorage.set(id, taskObj);
-    }
-    isAllowUrl(url) {
-        let isValid = false;
-        for (const path of this._allowPaths) {
-            if (url.endsWith(path)) {
-                isValid = true;
-            }
-        }
-        if (!isNaN(url.slice(-1))) {
-            isValid = true;
-        }
-        return isValid;
+        await task.execute();
     }
     extractId(url) {
-        const [, id] = url.split(/id=|&/);
-        return id;
+        const idPart = url.match(/(id|cmid)=\d+/);
+        if (!idPart)
+            throw new Error('invalid url');
+        return idPart[0].split('=').at(-1);
     }
-    fetchAssignData() {
+}
+class BaseTask {
+    _disallowPaths;
+    _id;
+    _task;
+    _cache;
+    constructor(id) {
+        this._disallowPaths = [];
+        this._id = id;
+    }
+    async loadCache() {
+        const cachesData = await LocalStorage.get();
+        this._cache = cachesData;
+    }
+    isValidUrl(url) {
+        for (const path of this._disallowPaths) {
+            if (new RegExp(path).test(url)) {
+                console.log('invalid url');
+                return false;
+            }
+        }
+        return true;
+    }
+    existsCache() {
+        if (!this._cache)
+            throw new Error('cache is not defined');
+        return this._cache.hasOwnProperty(this._id);
+    }
+}
+class Assign extends BaseTask {
+    constructor(id) {
+        super(id);
+        this._disallowPaths = ['action=editsubmission'];
+    }
+    async execute() {
+        if (!this.isValidUrl(location.href))
+            return;
+        await this.loadCache();
+        if (this.existsCache()) {
+            this._task = JSON.parse(this._cache[this._id]);
+        }
+        else {
+            this.fetch();
+        }
+        this.checkStatUpdate();
+        await LocalStorage.set(this._id, this._task);
+    }
+    fetch() {
         const taskName = document
             .querySelector('[role="main"]')
             ?.querySelector('h2')?.textContent;
-        console.log(taskName);
         const className = (() => {
             const text = document.querySelector('.page-header-headings')
                 ?.textContent;
             return text.slice(0, text.lastIndexOf(' '));
         })();
-        console.log(className);
         const deadline = (() => {
             const tbody = document
                 .querySelector('.generaltable')
@@ -89,33 +112,55 @@ class LMSManager {
             const time = date.slice(3).join();
             return day + ' ' + time;
         })();
-        const submissionState = (() => {
-            const tbody = document
-                .querySelector('.generaltable')
-                .querySelector('tbody');
-            const rawState = tbody?.children[0].querySelector('.c1')?.textContent;
-            return rawState.includes('提出済み') ? '提出済み' : '未提出';
-        })();
         const assign = {
             taskName: taskName,
             className: className,
             type: 'assign',
             deadline: deadline,
-            submissionState: submissionState,
+            submissionState: '未提出',
         };
-        return assign;
+        this._task = assign;
     }
-    fetchQuizData() {
+    checkStatUpdate() {
+        if (!this._task)
+            throw new Error('task is not defined');
+        const tbody = document
+            .querySelector('.generaltable')
+            .querySelector('tbody');
+        const rawState = tbody?.children[0].querySelector('.c1')?.textContent;
+        const submissionState = rawState.includes('提出済み')
+            ? '提出済み'
+            : '未提出';
+        this._task.submissionState = submissionState;
+    }
+}
+class Quiz extends BaseTask {
+    constructor(id) {
+        super(id);
+        this._disallowPaths = ['attempt.php'];
+    }
+    async execute() {
+        if (!this.isValidUrl(location.href))
+            return;
+        await this.loadCache();
+        if (this.existsCache()) {
+            this._task = JSON.parse(this._cache[this._id]);
+        }
+        else {
+            this.fetch();
+        }
+        this.checkStatUpdate();
+        await LocalStorage.set(this._id, this._task);
+    }
+    fetch() {
         const taskName = document
             .querySelector('[role="main"]')
             ?.querySelector('h2')?.textContent;
-        console.log(taskName);
         const className = (() => {
             const text = document.querySelector('.page-header-headings')
                 ?.textContent;
             return text.slice(0, text.lastIndexOf(' '));
         })();
-        console.log(className);
         const deadline = (() => {
             const info = document.querySelector('.quizinfo');
             const index = info.children.length - 2;
@@ -126,40 +171,24 @@ class LMSManager {
             const time = date.slice(4, 5).join();
             return day + ' ' + time;
         })();
-        const submissionState = (() => {
-            const table = document.querySelector('.generaltable');
-            if (!!table) {
-                return '提出済み';
-            }
-            else {
-                return '未提出';
-            }
-        })();
         const quiz = {
             taskName: taskName,
             className: className,
             type: 'quiz',
             deadline: deadline,
-            submissionState: submissionState,
+            submissionState: '未提出',
         };
-        return quiz;
+        this._task = quiz;
     }
-    checkStatUpdate(task) {
-        switch (task.type) {
-            case 'assign':
-                const tbody = document
-                    .querySelector('.generaltable')
-                    .querySelector('tbody');
-                const rawState = tbody?.children[0].querySelector('.c1')?.textContent;
-                const submissionState = rawState.includes('提出済み')
-                    ? '提出済み'
-                    : '未提出';
-                task.submissionState = submissionState;
-                break;
-            case 'quiz':
-                const table = document.querySelector('.generaltable');
-                task.submissionState = !!table ? '提出済み' : '未提出';
-                break;
+    checkStatUpdate() {
+        if (!this._task)
+            throw new Error('task is not defined');
+        if (/https:\/\/tlms.tsc.u-tokai.ac.jp\/mod\/quiz\/review.php\?attempt=\d+&cmid=\d+\S*/.test(location.href)) {
+            this._task.submissionState = '提出済み';
+        }
+        else {
+            const table = document.querySelector('.generaltable');
+            this._task.submissionState = !!table ? '提出済み' : '未提出';
         }
     }
 }
